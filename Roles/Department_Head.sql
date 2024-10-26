e5a14ff411b3504b1c9b7a48dc3679e65b35f535
Sandeep Kumar <kumar.sandeep439@gmail.com>
2024-10-26 16:24:45 +0530

    Add files via upload

---
+++ Roles/Department_Head.sql
@@ -0,0 +1,7 @@
+CREATE ROLE Department_Head
+
+GRANT SELECT ON EMS.Employees TO Department_Head
+GRANT SELECT ON EMS.Departments TO Department_Head
+
+
+
---
+++ Roles/Payroll_Staff.sql
@@ -0,0 +1,4 @@
+CREATE ROLE Payroll_Staff
+
+GRANT SELECT ON EMS.Employees TO Payroll_Staff
+GRANT SELECT ON EMS.Salaries TO Payroll_Staff
